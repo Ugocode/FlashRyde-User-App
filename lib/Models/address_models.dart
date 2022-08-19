64dab50a8c175952254a4c69e2152ea8b0b0069f
ugocode <obiohaugo5@gmail.com>
2022-08-19 10:32:48 +0100

    added these pages

---
+++ lib/Models/address_models.dart
@@ -0,0 +1,14 @@
+class Directions {
+  String? humanRadabableAddress;
+  String? locationName;
+  double? locationLatitude;
+  double? locationLongitude;
+  String? locationId;
+
+  Directions(
+      {this.humanRadabableAddress,
+      this.locationId,
+      this.locationLatitude,
+      this.locationLongitude,
+      this.locationName});
+}
---
+++ lib/Models/direction_details.dart
@@ -0,0 +1,14 @@
+class DirectionDetailsInfo {
+  int? distanceValue;
+  int? durationValue;
+  String? ePoints;
+  String? distanceText;
+  String? durationText;
+
+  DirectionDetailsInfo(
+      {this.distanceText,
+      this.distanceValue,
+      this.durationText,
+      this.durationValue,
+      this.ePoints});
+}
