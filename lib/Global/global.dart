ed591318af3f5238e226b51b01d0f4d9fbe1d5d6
ugocode <obiohaugo5@gmail.com>
2022-09-17 15:37:48 +0100

    change from driversRef to usersRef

--- lib/Global/global.dart
+++ lib/Global/global.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:fusers_app/Models/user_model.dart';
 
 final FirebaseAuth fireAuth = FirebaseAuth.instance;
 
-final DatabaseReference driversRef = FirebaseDatabase.instance.ref();
+final DatabaseReference usersRef =
+    FirebaseDatabase.instance.ref().child("users");
 
 User? currentFirebaseUser;
+
+//from our users model:
+UserModel? userModelCurrentInfo;
