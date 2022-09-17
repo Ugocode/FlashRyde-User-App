b699fed35313c17d4759abf244bd77e2c8cca1df
ugocode <obiohaugo5@gmail.com>
2022-09-17 15:38:26 +0100

    created activenearby drivers class

---
+++ lib/Models/active_nearby_available_drivers.dart
@@ -0,0 +1,8 @@
+class ActiveNearbyAvailableDrivers {
+  String? driverId;
+  double? locationLatitiude;
+  double? locationLongitiude;
+
+  ActiveNearbyAvailableDrivers(
+      {this.driverId, this.locationLatitiude, this.locationLongitiude});
+}
