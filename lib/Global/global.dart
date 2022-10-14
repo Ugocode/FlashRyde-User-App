60289a2318879e132534f3ac38258e0b6b3ce1c3
ugocode <obiohaugo5@gmail.com>
2022-10-14 19:05:46 +0100

    added direction details class

--- lib/Global/global.dart
+++ lib/Global/global.dart
@@ -2,12 +2,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fusers_app/Models/user_model.dart';
 
+import '../Models/direction_details.dart';
+
 final FirebaseAuth fireAuth = FirebaseAuth.instance;
 
 final DatabaseReference usersRef =
     FirebaseDatabase.instance.ref().child("users");
 
+final DatabaseReference driversRef =
+    FirebaseDatabase.instance.ref().child("drivers");
+
 User? currentFirebaseUser;
 
 //from our users model:
 UserModel? userModelCurrentInfo;
+
+// online / active drivers key infomation List:
+List dList = [];
+
+// to get our trip direction details info
+DirectionDetailsInfo? tripDirectiondetailsInfo;
